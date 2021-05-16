81c4fbb245b271626926269ade99bc0dc059560b
Michael Getz <mguitar24@gmail.com>
2021-05-15 22:03:22 -0700

    refactored api parsing logic into controller

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 import { useState, useEffect } from "react";
+import SearchPokemon from "./controller/SearchPokemon";
 import { pokemonRequest, genericPokemonAPIRequest } from "./api/pokemonRequest";
 import PokemonCards from "./components/PokemonCards";
 
 const Alert = (props) => {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
+};
+
+const PokeState = {
+  pokeImg: undefined,
+  pokeName: undefined,
+  pokeDescription: undefined,
 };
 
 const App = () => {
@@ -31,12 +38,6 @@ const App = () => {
     };
   });
 
-  const pokeState = {
-    pokeImg: undefined,
-    pokeName: undefined,
-    pokeDescription: undefined,
-  };
-
   const [pokemon, setPokemon] = useState([]);
   const [searchValue, setSearchValue] = useState("");
   const [snackBarOpen, setSnackBarOpen] = useState(false);
@@ -50,8 +51,8 @@ const App = () => {
     if (!searchValue) {
       return;
     }
-    const pokemonResult = await pokemonRequest(searchValue);
-    if (pokemonResult.status === 404) {
+    const pokemonResult = await SearchPokemon(searchValue, pokemon);
+    if (pokemonResult.pokemonStatus === 404) {
       if (snackBarOpen) {
         setSnackBarOpen(false);
       }
@@ -60,23 +61,11 @@ const App = () => {
       setSearchValue("");
       return;
     }
-    if (pokemonResult.data.name && pokemonResult.data.sprites) {
-      pokeState.pokeImg = pokemonResult.data.sprites.other["official-artwork"].front_default;
-      pokeState.pokeName = pokemonResult.data.name;
+
+    if (pokemonResult.pokeState) {
+      setPokemon([...pokemon, pokemonResult.pokeState]);
     }
-    if (pokemonResult.data.species) {
-      const speciesResult = await genericPokemonAPIRequest(pokemonResult.data.species.url);
-      let descriptions = speciesResult.data.flavor_text_entries;
-      descriptions = descriptions.filter((description) => description.language.name === "en");
-      descriptions = descriptions.slice(0, 5);
-      descriptions = descriptions.map((description) => description.flavor_text);
-      descriptions = descriptions.filter((description, index) => descriptions.indexOf(description) === index);
-      pokeState.pokeDescription = descriptions.join("");
-    }
-    const dupe = pokemon.find((poke) => poke.pokeName === pokeState.pokeName);
-    if (!dupe) {
-      setPokemon([...pokemon, pokeState]);
-    }
+
     setSearchValue("");
   };
 
@@ -112,3 +101,5 @@ const App = () => {
 };
 
 export default App;
+
+export { PokeState };
---
+++ src/controller/SearchPokemon.js
@@ -0,0 +1,29 @@
+import { pokemonRequest, genericPokemonAPIRequest } from "../api/pokemonRequest";
+import { PokeState as pokeState } from "../App";
+
+const SearchPokemon = async (searchValue, pokemon) => {
+  const pokemonResult = await pokemonRequest(searchValue);
+  const pokemonStatus = pokemonResult.status;
+
+  const dupe = pokemon.find((poke) => poke.pokeName === pokemonResult.data.name);
+  if (dupe || pokemonStatus === 404) {
+    return { pokemonStatus };
+  }
+
+  if (pokemonResult.data.name && pokemonResult.data.sprites) {
+    pokeState.pokeName = pokemonResult.data.name;
+    pokeState.pokeImg = pokemonResult.data.sprites.other["official-artwork"].front_default;
+  }
+  if (pokemonResult.data.species) {
+    const speciesResult = await genericPokemonAPIRequest(pokemonResult.data.species.url);
+    let descriptions = speciesResult.data.flavor_text_entries;
+    descriptions = descriptions.filter((description) => description.language.name === "en");
+    descriptions = descriptions.slice(0, 5);
+    descriptions = descriptions.map((description) => description.flavor_text);
+    descriptions = descriptions.filter((description, index) => descriptions.indexOf(description) === index);
+    pokeState.pokeDescription = descriptions.join("");
+  }
+  return { pokeState, pokemonStatus };
+};
+
+export default SearchPokemon;
