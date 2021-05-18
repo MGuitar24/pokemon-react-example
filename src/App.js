b64fcd2cae4d9e3b1a25968831d3f9c6b754448d
Michael Getz <mguitar24@gmail.com>
2021-05-17 21:19:46 -0700

    added search component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import { useState, useEffect } from "react";
-import SearchPokemon from "./controller/SearchPokemon";
+import { useState } from "react";
 import PokemonCards from "./components/PokemonCards";
-import originalPokemon from "./controller/originalPokemon";
 import { QueryClient, QueryClientProvider } from "react-query";
+import Search from "./components/Search";
 
 const queryClient = new QueryClient();
 
@@ -17,97 +13,34 @@ const Alert = (props) => {
 };
 
 const App = () => {
-  const [pokemon, setPokemon] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
   const [snackBarOpen, setSnackBarOpen] = useState(false);
   const [noResultSearchValue, setNoResultSearchValue] = useState("");
-
-  const enterPressed = (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      onSearch(searchValue);
-    }
-  };
+  const [pokemon, setPokemon] = useState([]);
 
   const handleCloseSnackBar = () => {
     setSnackBarOpen(false);
     setNoResultSearchValue("");
   };
 
-  useEffect(() => {
-    window.addEventListener("keydown", enterPressed);
-    return () => {
-      window.removeEventListener("keydown", enterPressed);
-    };
-  });
-
-  const updateValue = (event, value, reason) => {
-    setSearchValue(value.toLowerCase());
-  };
-
-  const onSearch = async () => {
-    if (!searchValue) {
-      return;
-    }
-    const pokemonResult = await SearchPokemon(searchValue, pokemon);
-    if (pokemonResult.pokemonStatus === 404) {
-      if (snackBarOpen) {
-        setSnackBarOpen(false);
-      }
-      setNoResultSearchValue(searchValue);
-      setSnackBarOpen(true);
-      setSearchValue("");
-      return;
-    }
-
-    if (pokemonResult.pokeState) {
-      setPokemon([...pokemon, pokemonResult.pokeState]);
-    }
-
-    setSearchValue("");
-  };
-
   return (
     <>
-      <div className="App">
-        <Snackbar
-          anchorOrigin={{ vertical: "top", horizontal: "center" }}
-          open={snackBarOpen}
-          autoHideDuration={2000}
-          onClose={handleCloseSnackBar}
-        >
-          <Alert onClose={handleCloseSnackBar} severity="error">
-            No result found for search {noResultSearchValue}.
-          </Alert>
-        </Snackbar>
-        <Autocomplete
-          style={{ width: 300, display: "inline-block", verticalAlign: "top" }}
-          freeSolo
-          id="free-solo"
-          inputValue={searchValue}
-          onInputChange={updateValue}
-          disableClearable
-          options={originalPokemon}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Search pokemon"
-              variant="outlined"
-              margin="normal"
-              InputProps={{ ...params.InputProps, type: "search" }}
-            />
-          )}
-        />
-        <Button
-          style={{ height: 56, marginTop: 15, marginLeft: 15 }}
-          onClick={onSearch}
-          variant="contained"
-          color="primary"
-        >
-          Search
-        </Button>
-      </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={snackBarOpen}
+        autoHideDuration={2000}
+        onClose={handleCloseSnackBar}
+      >
+        <Alert onClose={handleCloseSnackBar} severity="error">
+          No result found for search {noResultSearchValue}.
+        </Alert>
+      </Snackbar>
       <QueryClientProvider client={queryClient}>
+        <Search
+          pokemon={pokemon}
+          setPokemon={setPokemon}
+          setNoResultSearchValue={setNoResultSearchValue}
+          setSnackBarOpen={setSnackBarOpen}
+        />
         <PokemonCards pokemon={pokemon} setPokemon={setPokemon} />
       </QueryClientProvider>
     </>
---
+++ src/components/Search.js
@@ -0,0 +1,81 @@
+import "./Search.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { useState, useEffect } from "react";
+import originalPokemon from "../controller/originalPokemon";
+import SearchPokemon from "../controller/SearchPokemon";
+
+const Search = ({ pokemon, setPokemon, setNoResultSearchValue, setSnackBarOpen }) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const updateValue = (event, value, reason) => {
+    setSearchValue(value.toLowerCase());
+  };
+
+  const enterPressed = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      onSearch(searchValue);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", enterPressed);
+    return () => {
+      window.removeEventListener("keydown", enterPressed);
+    };
+  });
+
+  const onSearch = async () => {
+    if (!searchValue) {
+      return;
+    }
+    const pokemonResult = await SearchPokemon(searchValue, pokemon);
+    if (pokemonResult.pokemonStatus === 404) {
+      setNoResultSearchValue(searchValue);
+      setSnackBarOpen(true);
+      setSearchValue("");
+      return;
+    }
+
+    if (pokemonResult.pokeState) {
+      setPokemon([...pokemon, pokemonResult.pokeState]);
+    }
+
+    setSearchValue("");
+  };
+
+  return (
+    <div className="Search">
+      <Autocomplete
+        style={{ width: 300, display: "inline-block", verticalAlign: "top" }}
+        freeSolo
+        id="free-solo"
+        inputValue={searchValue}
+        onInputChange={updateValue}
+        disableClearable
+        options={originalPokemon}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search pokemon"
+            variant="outlined"
+            margin="normal"
+            InputProps={{ ...params.InputProps, type: "search" }}
+          />
+        )}
+      />
+      <Button
+        style={{ height: 56, marginTop: 15, marginLeft: 15 }}
+        onClick={onSearch}
+        variant="contained"
+        color="primary"
+      >
+        Search
+      </Button>
+    </div>
+  );
+};
+
+export default Search;
