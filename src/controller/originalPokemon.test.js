67fdb388dba2a712a108f44dddd12b13d089f5fd
Mike <mguitar24@gmail.com>
2021-05-19 08:14:42 -0700

    removing cra tests (commenting for future reference), adding static pokemon array validation test

--- src/controller/originalPokemon.test.js
+++ src/controller/originalPokemon.test.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 import originalPokemon from "./originalPokemon";
 
+jest.setTimeout(50000);
+
 test("static pokemon list contains valid results", async () => {
-  for (const pokemon in originalPokemon) {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`, { timeout: 3000 });
-    expect(response.status).toEqual(200);
+  for (let pokemon of originalPokemon) {
+    try {
+      pokemon = pokemon.toLowerCase();
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, { timeout: 3000 });
+      expect(response.status).toEqual(200);
+    } catch (error) {
+      console.log(error);
+      expect(error).toBeNull();
+    }
   }
 });
